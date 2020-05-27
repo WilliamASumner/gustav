f00f725a3023a35bfff2f027ce9d74ae3a00ba1d
Will Sumner <williamasumner@gmail.com>
2020-05-27 12:45:31 -0400

    Move AJAX processing to separate function

--- gustav/forms/nafc_html/nafc_html.py
+++ gustav/forms/nafc_html/nafc_html.py
@@ -36,6 +36,58 @@ else:
     import socketserver as sserver
     import http.server as server_lib
 
+def process_ajax(interface,data_string):
+    """ Parse and then respond to AJAX request
+
+    JSON Events are sent as follows:
+
+    From Browser: { 'EventType': 'KeyPress or Poll',
+                    'Value'    : 'KeyCode or Null',
+                    'Time'     : 'Timestamp or Null' }
+
+    To Browser:   'result': { 'Commands' : '{
+                      'Command':
+                        0  - show_notify_left
+                        1  - show_notify_right
+                        2  - update_notify_left
+                        3  - update_notify_right
+                        4  - update_status_left
+                        5  - update_status_right
+                        6  - update_status_center
+                        7  - update_title_left
+                        8  - update_title_right
+                        9  - update_title_center
+                        10 - show_buttons
+                        11 - show_prompt
+                        12 - set_border
+                        13 - set_color
+                        14 - quit',
+                     'Value' : 'Hex Color or Text',
+                     'IDs' : 'Alternative Name (e.g. A, B, C)
+                   }
+                  }
+    """
+
+    response_str = "Error processing client JSON"
+
+    try:
+        template = '{ "result" : %s }'
+        data = json.loads(data_string)
+
+        if data['EventType'] == 'KeyPress':
+            interface.set_key(data['Value'])
+            response_str = template % ('"KeyPressReceived"')
+
+        elif data['EventType'] == 'Poll':
+            response_str = template % interface.cmd_queue.gen_cmdstr()
+
+    except Exception as e:
+        print("Encountered exception while processing AJAX JSON: " + str(e))
+
+    finally:
+        return response_str
+
+
 
 # Adapted from https://gist.github.com/bradmontgomery/2219997
 class CustomRequestHandler(server_lib.SimpleHTTPRequestHandler):
@@ -68,57 +120,7 @@ class CustomRequestHandler(server_lib.SimpleHTTPRequestHandler):
         self.end_headers()
 
     def process_json(self,data_string):
-        """ Parse and then respond to AJAX request
-
-        JSON Events are sent as follows:
-
-        From Browser: { 'EventType': 'KeyPress or Poll',
-                        'Value'    : 'KeyCode or Null',
-                        'Time'     : 'Timestamp or Null' }
-
-        To Browser:   'result': { 'Commands' : '{
-                          'Command':
-                            0  - show_notify_left
-                            1  - show_notify_right
-                            2  - update_notify_left
-                            3  - update_notify_right
-                            4  - update_status_left
-                            5  - update_status_right
-                            6  - update_status_center
-                            7  - update_title_left
-                            8  - update_title_right
-                            9  - update_title_center
-                            10 - show_buttons
-                            11 - show_prompt
-                            12 - set_border
-                            13 - set_color
-                            14 - quit',
-                         'Value' : 'Hex Color or Text',
-                         'IDs' : 'Alternative Name (e.g. A, B, C)
-                       }
-                      }
-        """
-
-        response_str = "Error processing client JSON"
-
-        try:
-            template = '{ "result" : %s }'
-            data = json.loads(data_string)
-
-            if data['EventType'] == 'KeyPress':
-                self.interface.set_key(data['Value'])
-                response_str = template % ('"Success"')
-
-            elif data['EventType'] == 'Poll':
-                response_str = template % self.interface.cmd_queue.gen_cmdstr()
-
-        except Exception as e:
-            print("Encountered exception: " + str(e))
-
-        finally:
-            return response_str
-
-        return json.loads(data_string)
+        return process_ajax(self.interface,data_string)
 
     def do_GET(self):
         """
