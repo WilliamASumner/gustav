c4bc32741af983fb0700b0eb41c302f92892d06b
Will Sumner <williamasumner@gmail.com>
2020-05-27 13:11:47 -0400

    Separate local server into new file

---
+++ gustav/forms/nafc_html/local_server.py
@@ -0,0 +1,121 @@
+import sys
+import re
+import threading
+import webbrowser
+from ajax import process_ajax
+
+if sys.version_info[0] == 2:
+    import SocketServer as sserver
+    import SimpleHTTPServer as server_lib
+else:
+    import socketserver as sserver
+    import http.server as server_lib
+
+SockError = sserver.socket.error
+
+# Adapted from https://gist.github.com/bradmontgomery/2219997
+class CustomRequestHandler(server_lib.SimpleHTTPRequestHandler):
+    interface = None
+    logging = False
+
+    def connect_interface(self,InterfaceInstance):
+        self.interface = InterfaceInstance
+
+    # https://stackoverflow.com/questions/25360798/save-logs-simplehttpserver
+    def log_message(self, format, *args):
+        """
+        Method for writing out errors/log messages
+        """
+        if self.logging:
+            log_file = open('logfile.txt', 'a', 1) # output not needed
+            log_file.write(
+                "%s - - [%s] %s\n" % (self.client_address[0],
+                    self.log_date_time_string(),
+                    format%args))
+
+    def set_response_headers(self,kind):
+        self.send_response(200)
+        self.send_header("Content-type",kind)
+        self.end_headers()
+
+    def set_ajax_headers(self):
+        self.send_response(200)
+        self.send_header("Content-type","application/json")
+        self.end_headers()
+
+    def process_json(self,data_string):
+        return process_ajax(self.interface,data_string)
+
+    def do_GET(self):
+        """
+        Responds to all HTTP GET requests for things like html, css, js
+        """
+
+        if re.match("^/$",self.path) or re.match("/index.html",self.path):
+            self.set_response_headers("text/html")
+            self.wfile.write(self.interface.generate_html())
+
+        elif re.match(".*css",self.path):
+            self.set_response_headers("text/css")
+            self.wfile.write(self.interface.generate_css())
+
+        elif re.match(".*js",self.path):
+            self.set_response_headers("application/javascript")
+            self.wfile.write(self.interface.generate_js())
+
+    def do_POST(self):
+        """
+        Responds to all HTTP POST requests that send data here
+        """
+
+        length = int(self.headers['Content-Length'])
+        data_string = self.rfile.read(length).decode('UTF-8')
+        response_str = self.process_json(data_string)
+        self.set_ajax_headers();
+
+        self.wfile.write(bytearray(response_str,'UTF-8'))
+
+class CustomTCPServer(sserver.TCPServer,object):
+    def __init__(self,server_address,RequestHandler,InterfaceInstance,logging=False):
+        RequestHandler.interface = InterfaceInstance
+        RequestHandler.logging = logging
+        super(CustomTCPServer,self).__init__(server_address,RequestHandler)
+
+class LocalServer:
+    def __init__(self, interface):
+        self.interface = interface
+        self.port = 8000
+        self.server_thread = threading.Thread(target=self.start_server_thread) # start server in background
+
+    def start_server_thread(self):
+            print("Serving on port",self.port)
+            try:
+                self.server.serve_forever()
+
+            except Exception as e:
+                print("Unexpected error on server:" + str(e))
+                self.server.server_close()
+                sys.exit(1) # Don't continue on an error
+
+    def start(self, openBrowser=True):
+        while True:
+            try:
+                self.server = CustomTCPServer(("",self.port),CustomRequestHandler,self.interface)
+                break
+            except (OSError,SockError):
+                self.port += 1 # port in use
+            except Exception as e:
+                raise Exception("Error starting up server: " + str(e))
+
+        if openBrowser:
+            self.browser = webbrowser.get() # get befault browser
+            self.browser.open("http://localhost:" + str(self.port))
+        self.server_thread.setDaemon(True)
+        self.server_thread.start()
+
+
+
+
+
+
+
--- gustav/forms/nafc_html/nafc_html.py
+++ gustav/forms/nafc_html/nafc_html.py
@@ -23,102 +23,13 @@
 import os,sys
 import time
 import ctypes
-import webbrowser
-import threading
 from string import Template
-import re
-import json
 
-from ajax import process_ajax, CommandQueue
-
-if sys.version_info[0] == 2:
-    import SocketServer as sserver
-    import SimpleHTTPServer as server_lib
-else:
-    import socketserver as sserver
-    import http.server as server_lib
-
-# Adapted from https://gist.github.com/bradmontgomery/2219997
-class CustomRequestHandler(server_lib.SimpleHTTPRequestHandler):
-    interface = None
-    logging = False
-
-    def connect_interface(self,InterfaceInstance):
-        self.interface = InterfaceInstance
-
-    # https://stackoverflow.com/questions/25360798/save-logs-simplehttpserver
-    def log_message(self, format, *args):
-        """
-        Method for writing out errors/log messages
-        """
-        if self.logging:
-            log_file = open('logfile.txt', 'a', 1) # output not needed
-            log_file.write(
-                "%s - - [%s] %s\n" % (self.client_address[0],
-                    self.log_date_time_string(),
-                    format%args))
-
-    def set_response_headers(self,kind):
-        self.send_response(200)
-        self.send_header("Content-type",kind)
-        self.end_headers()
-
-    def set_ajax_headers(self):
-        self.send_response(200)
-        self.send_header("Content-type","application/json")
-        self.end_headers()
-
-    def process_json(self,data_string):
-        return process_ajax(self.interface,data_string)
-
-    def do_GET(self):
-        """
-        Responds to all HTTP GET requests for things like html, css, js
-        """
-
-        if re.match("^/$",self.path) or re.match("/index.html",self.path):
-            self.set_response_headers("text/html")
-            self.wfile.write(self.interface.generate_html())
-
-        elif re.match(".*css",self.path):
-            self.set_response_headers("text/css")
-            self.wfile.write(self.interface.generate_css())
-
-        elif re.match(".*js",self.path):
-            self.set_response_headers("application/javascript")
-            self.wfile.write(self.interface.generate_js())
-
-    def do_POST(self):
-        """
-        Responds to all HTTP POST requests that send data here
-        """
-
-        length = int(self.headers['Content-Length'])
-        data_string = self.rfile.read(length).decode('UTF-8')
-        response_str = self.process_json(data_string)
-        self.set_ajax_headers();
-
-        self.wfile.write(bytearray(response_str,'UTF-8'))
-
-class CustomTCPServer(sserver.TCPServer,object):
-    def __init__(self,server_address,RequestHandler,InterfaceInstance,logging=False):
-        RequestHandler.interface = InterfaceInstance
-        RequestHandler.logging = logging
-        super(CustomTCPServer,self).__init__(server_address,RequestHandler)
-
+from ajax import CommandQueue
+from local_server import CustomTCPServer, CustomRequestHandler, LocalServer, SockError
 
 class Interface():
-    def start_server_thread(self):
-        print("Serving on port",self.port)
-        try:
-            self.server.serve_forever()
-
-        except Exception as e:
-            print("Unexpected error on server:" + str(e))
-            self.server.server_close()
-            sys.exit(1) # Don't continue on an error
-
-    def __init__(self,alternatives=2, prompt="Choose an alternative"):
+    def __init__(self,alternatives=2, prompt="Choose an alternative", isServingLocal=True):
         self.title_l_str = "Gustav n-AFC!"
         self.title_c_str = ""
         self.title_r_str = ""
@@ -155,23 +66,11 @@ class Interface():
         self.button_color_names = ['None', 'Grey', 'Green', 'Red', 'Yellow'] # Allow user to specify color/border by name
         self.button_border_names = ['None', 'Light', 'Heavy', 'Double']      # Must be in same order as button_f_colors 
 
-        self.port = 8000
-        while True:
-            try:
-                self.server = CustomTCPServer(("",self.port),CustomRequestHandler,self)
-                break
-            except (OSError,sserver.socket.error):
-                self.port += 1
-            except Exception as e:
-                print("Error starting up server: " + str(e))
-                sys.exit(1)
+        self.isServingLocal = isServingLocal
 
-        #TODO clean this up
-        self.browser = webbrowser.get() # get befault browser
-        self.browser.open("http://localhost:" + str(self.port))
-        self.server_thread = threading.Thread(target=self.start_server_thread) # start server in background
-        self.server_thread.setDaemon(True)
-        self.server_thread.start()
+        if self.isServingLocal:
+            self.local_server = LocalServer(self).start()
+
 
     def destroy(self):
         self.cmd_queue.quit()
